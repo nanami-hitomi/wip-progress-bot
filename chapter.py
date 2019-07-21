4712d826c4dc63c5d74fe0ec599416973f632ee0
Sakurako <43500169+Kuruby@users.noreply.github.com>
2019-07-20 21:13:47 -0400

    Python classes for sql tables

---
+++ chapter.py
@@ -0,0 +1,18 @@
+class Chapter:
+    def __init__(self, Manga, chapter_number):
+        self.related_manga_id=Manga.manga_id
+        self.chapter_number=chapter_number
+        
+        self.translator = Manga.translator
+        self.proofreader = Manga.proofreader
+        self.redrawer = Manga.redrawer
+        self.typesetter = Manga.typesetter
+        self.qc = Manga.qc
+
+        self.translation_complete = 0
+        self.proofread_complete = 0
+        self.redraw_complete = 0
+        self.typeset_complete = 0
+        self.qc_complete = 0
+
+        self.uploaded = 0
---
+++ manga.py
@@ -0,0 +1,15 @@
+class Manga:
+    def __init__(self, manga_id, nickname, full_name = None, translator = None,
+                proofreader = None, redrawer = None, typesetter = None, qc = None):
+        
+        self.manga_id=manga_id
+        self.full_name = full_name
+        self.nickname = nickname
+        
+        self.translator = translator
+        self.proofreader = proofreader
+        self.redrawer = redrawer
+        self.typesetter = typesetter
+        self.qc = qc
+
+        self.abandoned = 0
